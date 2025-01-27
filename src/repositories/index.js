98ce923cdba29c603fcf306d57ba60265fe3a8d8
John Espitia <jespitiasa@outlook.com>
2025-01-27 14:49:23 -0500

    Del Repositories

--- src/repositories/index.js
+++
@@ -1,8 +0,0 @@
-import { getUserDAO, getProductDAO, getCartDAO } from "../dao/factory.js";
-import UserRepository from "./user.repository.js";
-import ProductRepository from "./product.repository.js";
-import CartRepository from "./cart.repository.js";
-
-export const userRepository = new UserRepository(getUserDAO());
-export const productRepository = new ProductRepository(getProductDAO());
-export const cartRepository = new CartRepository(getCartDAO());
